3dd970ce1a389de1b720775fe3b07de03f4f6286
pavlik <readystores.ru@gmail.com>
2026-06-18 17:31:49 +0300

    feat(account): add user model

--- account/models.py
+++ account/models.py
@@ -1,1 +1,37 @@
-# Create your models here.
+from typing import Any
+from typing import Self
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self: Self,
+        email: str,
+        password: str | None = None,
+        **extra_fields: Any,
+    ) -> Self:
+        if not email:
+            raise ValueError(_("The Email field is not set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    username = None
+    middle_name = models.CharField(max_length=64, blank=True)
+    email = models.EmailField(_("email address"), unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self: Self) -> str:
+        return self.email
--- em_system/settings.py
+++ em_system/settings.py
@@ -30,6 +30,8 @@ class Base(Configuration):
         "django.contrib.sessions",
         "django.contrib.messages",
         "django.contrib.staticfiles",
+        "rest_framework",
+        "account",
     ]
 
     MIDDLEWARE: list[str] = [
@@ -115,6 +117,22 @@ class Base(Configuration):
     # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+    AUTH_USER_MODEL = "account.User"
+
+    # DRF
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": (
+            "auth.authentication.PasetoAuthentication",
+        ),
+        "DEFAULT_PERMISSION_CLASSES": (
+            "rest_framework.permissions.IsAuthenticated",
+        ),
+        "DEFAULT_PAGINATION_CLASS": (
+            "rest_framework.pagination.PageNumberPagination"
+        ),
+        "PAGE_SIZE": 8,
+    }
 
 
 class Development(Base):
