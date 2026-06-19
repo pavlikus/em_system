fa66e1545f3d98ed5273199a823686b6e8057dc5
pavlik <readystores.ru@gmail.com>
2026-06-19 12:13:17 +0300

    feat(account): add logout endpoint

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from account.views import LoginView
+from account.views import LogoutView
 from account.views import RegistrationView
 
 app_name = "account"
@@ -9,4 +10,5 @@ app_name = "account"
 urlpatterns = [
     path("registration/", RegistrationView.as_view(), name="registration"),
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,18 +1,21 @@
 from typing import Any
 from typing import Self
 
+from django.utils.translation import gettext_lazy as _
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from account.serializers import LoginSerializer
 from account.serializers import RegistrationSerializer
 from account.serializers import UserDetailsSerializer
 from auth.tokens import Token
 from auth.utils import set_cookies
+from auth.utils import unset_cookies
 
 
 class RegistrationView(CreateAPIView):
@@ -40,3 +43,19 @@ class LoginView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         self.user = serializer.validated_data["user"]
         return self.get_response()
+
+
+class LogoutView(APIView):
+    permission_classes = (AllowAny,)
+    http_method_names = ("post", "options", "head")
+
+    def post(
+        self: Self, request: Request, *args: Any, **kwargs: Any
+    ) -> Response:
+        response = Response(
+            {"detail": _("Successfully logged out.")},
+            status=status.HTTP_200_OK,
+        )
+        unset_cookies(response)
+
+        return response
